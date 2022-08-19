49abdb0608ca4e269dd1b829604a362a2ae6ef1d
hammadaslam10m <muhammadaslambhai@gmail.com>
2022-08-19 13:25:25 +0500

    done

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Slider from './Slider'
+import Footer from './Footer'
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Slider/>
+<Footer/>
+    </>
+  )
+}
